f518eb8955e81206db977bd96c8a3a486acc38f9
Cory Francis Myers <cfm@panix.com>
2016-03-03 15:50:37 -0500

    QueueConnection.bind.handle_message(): Pass entire "method" object to callback.

--- txeasypika/__init__.py
+++ txeasypika/__init__.py
@@ -234,7 +234,7 @@ class QueueConnection(object):
                 channel.basic_ack(method.delivery_tag)
                 return
 
-            callback(ChannelProxy(channel), method.delivery_tag, header, message)
+            callback(ChannelProxy(channel), method, header, message)
 
         logger.debug("Binding %s to %s/%s" % (queue_name, exchange, routing_key))
 
